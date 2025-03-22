a42e388eacb5ff938f633b505063cb6abe13bf7e
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-03-22 16:02:19 +0800

    Reversing case format and printing the result

--- printing_name_in_reverse_casing_format.py
+++ printing_name_in_reverse_casing_format.py
@@ -1,2 +1,11 @@
 fullname = input("Enter your fullname: ")
 
+reversed_casing = ""
+
+for char in fullname:
+    if char.islower():
+        reversed_casing += char.upper()
+    else:
+        reversed_casing += char.lower()
+
+print("Output:", reversed_casing)
