a1f282a65d3619471b36f23a924b48fe99453c93
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-03-22 15:55:36 +0800

    Pseudocode

---
+++ printing_name_in_reverse_casing_format.py
@@ -0,0 +1,7 @@
+# Pseudocode
+    # Asking the user for their fullname
+    # If a character in their name is in uppercase
+        # Convert it to lowercase
+    # Else
+        # Convert it to uppercase
+    # Print the name
