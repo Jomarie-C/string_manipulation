6cada4d2a163c7f018a7c0e566cb9e4651493652
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-03-22 15:56:37 +0800

    Asking the user to enter their fullname

--- printing_name_in_reverse_casing_format.py
+++ printing_name_in_reverse_casing_format.py
@@ -1,7 +1,2 @@
-# Pseudocode
-    # Asking the user for their fullname
-    # If a character in their name is in uppercase
-        # Convert it to lowercase
-    # Else
-        # Convert it to uppercase
-    # Print the name
+fullname = input("Enter your fullname: ")
+
