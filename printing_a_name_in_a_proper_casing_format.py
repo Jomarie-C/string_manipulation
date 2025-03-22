ea8c6af4d2f781967e31976c0b029cc76f00762d
Jomarie Cagurungan <cagurunganjomarie6@gmail.com>
2025-03-22 15:46:47 +0800

    Asking the user to enter their name

--- printing_a_name_in_a_proper_casing_format.py
+++ printing_a_name_in_a_proper_casing_format.py
@@ -1,4 +1,1 @@
-# Pseudocode
-    # Ask the user to enter their name
-    # If the user entered a name with incorrect casing
-        # Convert it to a proper casing format
+fullname = input("Enter your fullname: ")
